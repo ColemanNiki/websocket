920b1d8abb18d8cadc02f7ff93594f3a74d88c52
ColemanNiki <nitanmao@qq.com>
2017-03-22 23:40:18 +0800

    直播密匙生成

--- test/database/models.js
+++ test/database/models.js
@@ -2,5 +2,11 @@ module.exports={
     users:{
         name:{type:String,required:true},
         pwd:{type:String,required:true}
+    },
+    lives:{
+        name:{type:String,required:true},
+        beused:{type:Boolean,required:true},
+        startTime:{type:Date,required:false},
+        endTime:{type:Date,required:false},
     }
 };
--- test/routes/users.js
+++ test/routes/users.js
@@ -9,7 +9,33 @@ router.get('/getId',function(req,res){
   if(!req.session.user)
     res.json({success:0,message:400});
   else{
-    res.json({success:1,message:{id:req.session.user.id}});
+    console.log("getId");
+    var lives = global.dbHandel.getModel('lives');
+    console.log(lives);    
+    var name = req.session.user.name;
+    console.log("start find-----------------------------------------------");
+    lives.findOne({name:name,beused:false},function(err,doc){
+      console.log("start find");
+      if(err){
+        res.json({success:0,message:501});
+      }
+      else{
+        if(!doc){
+          var live = new lives({
+            name:name,
+            beused:false
+         });
+          live.save(function(err,doc){
+            if(err)
+              res.json({success:0,message:501});
+            else
+              res.json({success:1,message:{name:name,key:doc._id}});
+          });
+        }
+        else
+          res.json({success:1,message:{name:doc.name,key:doc._id}});
+      }
+    });
   }
 })
 module.exports = router;
