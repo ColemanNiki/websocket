a7fe7e44f5b073f423cafee91fcb36c7ffe81535
ColemanNiki <nitanmao@qq.com>
2017-03-25 16:47:41 +0800

    stream模块分离

---
+++ test/stream.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var stream = express();
+stream.send = null;
+stream.use(function(request,response){
+    console.log(request.url);
+    var params = request.url.substr(1).split('/');
+
+	if (params[0] !== 'secret') {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + ':' +
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+  console.log("streamServer come");
+  response.connection.setTimeout(0);
+  request.on('data',function(data){
+    stream.send();
+  });
+});
+
+module.exports = stream;
+
