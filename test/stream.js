85151fb08cfca454447219a69d5d8d71db18f3aa
ColemanNiki <nitanmao@qq.com>
2017-03-26 12:24:56 +0800

    ssl控制  直播密钥控制

--- test/stream.js
+++ test/stream.js
@@ -1,22 +1,51 @@
 var express = require('express');
+var url = require('url');
 var stream = express();
 stream.send = null;
 stream.use(function(request,response){
-    console.log(request.url);
-    var params = request.url.substr(1).split('/');
-
-	if (params[0] !== 'secret') {
-		console.log(
+  var arg =   url.parse(request.url,true).query;
+  console.log(arg);
+  if(arg.name == null || arg.key == null){
+    console.log(
 			'Failed Stream Connection: '+ request.socket.remoteAddress + ':' +
 			request.socket.remotePort + ' - wrong secret.'
 		);
 		response.end();
-	}
-  console.log("streamServer come");
-  response.connection.setTimeout(0);
-  request.on('data',function(data){
-    stream.send(data);
-  });
+  }
+  else{
+    var lives = global.dbHandel.getModel('lives');
+    lives.findOne({name:arg.name,_id:arg.key,beused:false},function(err,doc){
+      console.log("has value");
+      if(err){
+        response.send(404);
+      }
+      else{
+        if(doc != null){
+          console.log(doc);
+          console.log("streamServer come");
+          response.connection.setTimeout(0);
+          doc.beused = true;
+          lives.update({_id:arg.key},{beused:true},function(error){});
+          request.on('data',function(data){
+            stream.send(data);
+          });
+        }
+      }
+    })
+  }
+  // var params = request.url.substr(1).split('/');
+	// if (params[0] !== 'secret') {
+	// 	console.log(
+	// 		'Failed Stream Connection: '+ request.socket.remoteAddress + ':' +
+	// 		request.socket.remotePort + ' - wrong secret.'
+	// 	);
+	// 	response.end();
+	// }
+  // console.log("streamServer come");
+  // response.connection.setTimeout(0);
+  // request.on('data',function(data){
+  //   stream.send(data);
+  // });
 });
 
 module.exports = stream;
